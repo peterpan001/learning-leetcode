3835b5b8458a66102276595d6cfbc5514ec8f05e
panli004 <panli004@ke.com>
2021-12-11 19:47:15 +0800

    [LEETCODE-00006]-sword offer：从上到下打印二叉树 I && II && III

---
+++ learning-sword-offer/src/main/java/com/panli/sword/offer/common/TreeNode.java
@@ -0,0 +1,20 @@
+package com.panli.sword.offer.common;
+
+/**
+ * 二叉树类
+ *
+ * @author lipan
+ * @date 2021-12-11
+ */
+public class TreeNode {
+
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
