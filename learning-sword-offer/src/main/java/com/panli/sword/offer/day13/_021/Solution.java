d5eb46179958707d90822469ede37c1e2999c552
panli004 <panli004@ke.com>
2021-12-16 08:27:52 +0800

    [LEETCODE-00013]-sword offer：调整数组顺序使奇数位于偶数前面&和为s的两个数字&反转单词顺序

---
+++ learning-sword-offer/src/main/java/com/panli/sword/offer/day13/_021/Solution.java
@@ -0,0 +1,35 @@
+package com.panli.sword.offer.day13._021;
+
+/**
+ * 调整数组顺序使奇数位于偶数前面
+ *
+ * @author lipan
+ * @date 2021-12-15
+ */
+public class Solution {
+
+    /**
+     * 左右指针解决此问题
+     *
+     * @param nums
+     * @return
+     */
+    public int[] exchange(int[] nums) {
+        if (nums == null || nums.length == 1) { // 数据为空或者数组长度为1直接返回
+            return nums;
+        }
+        int left = 0, right = nums.length - 1; // 声明左右指针
+        while (left < right) { // 临界条件：左指针小于右指针
+            while (left < right && nums[left] % 2 == 1) { // 左指针小于右指针且左指针元素为奇数，左指针++
+                left++;
+            }
+            while (left < right && nums[right] % 2 == 0) { // 左指针小于右指针且右指针元素为偶数，右指针--
+                right--;
+            }
+            int tmp = nums[left]; // 左右交换位置
+            nums[left] = nums[right];
+            nums[right] = tmp;
+        }
+        return nums;
+    }
+}
---
+++ learning-sword-offer/src/main/java/com/panli/sword/offer/day13/_057/Solution.java
@@ -0,0 +1,28 @@
+package com.panli.sword.offer.day13._057;
+
+/**
+ * 和为 s 的两个数字
+ *
+ * @author lipan
+ * @date 2021-12-15
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 1 || nums[0] >= target) { // 临界条件
+            return new int[0];
+        }
+
+        int left = 0, right = nums.length - 1; // 声明左右指针
+        while (left < right) { // 临界条件：左指针小于右指针
+            if (nums[left] + nums[right] == target) { // 如果左指针元素加右指针元素等于目标值，直接返回
+                return new int[]{nums[left], nums[right]};
+            } else if (nums[left] + nums[right] > target) { // 如果大于目标值，则右指针--
+                right--;
+            } else { // 否则左指针++
+                left++;
+            }
+        }
+        return new int[0]; // 不存在这样的两个元素满足target值
+    }
+}
