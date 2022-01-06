8de0dfcbe5a7c296203c0812e0e582efb761b986
panli004 <panli004@ke.com>
2022-01-06 08:09:05 +0800

    [LEETCODE-00020]-sword offer：重建二叉树&数值的整数次方&二叉搜索树的后序遍历序列

---
+++ learning-sword-offer/src/main/java/com/panli/sword/offer/day20/_007/Solution.java
@@ -0,0 +1,49 @@
+package com.panli.sword.offer.day20._007;
+
+import com.panli.sword.offer.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 重建二叉树
+ *
+ * @author lipan
+ * @date 2021-12-29
+ */
+public class Solution {
+
+    /**
+     * 迭代
+     * 前序遍历：根左右
+     * 中序遍历：左根右
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree1(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null) {
+            return null;
+        }
+        List<Integer> preList = new ArrayList<>();
+        List<Integer> inList = new ArrayList<>();
+        for (int i = 0; i < preorder.length; i++) {
+            preList.add(preorder[i]);
+            inList.add(inorder[i]);
+        }
+        return buildTreeHandle(preList, inList);
+    }
+
+    private TreeNode buildTreeHandle(List<Integer> preList, List<Integer> inList) {
+        if (inList.size() == 0) {
+            return null;
+        }
+        int rootVal = preList.remove(0);
+        int mid = inList.indexOf(rootVal);
+        TreeNode root = new TreeNode(rootVal);
+        root.left = buildTreeHandle(preList, inList.subList(0, mid));
+        root.right = buildTreeHandle(preList, inList.subList(mid + 1, inList.size()));
+        return root;
+    }
+}
