d52bbaf35380daddd634a90cff2bdaefd87f3d2f
panli004 <panli004@ke.com>
2022-02-13 11:34:23 +0800

    [LEETCODE-00027]-sword offer：滑动窗口的最大值&队列的最大值

---
+++ learning-sword-offer/src/main/java/com/panli/sword/offer/day27/_059II/MaxQueue.java
@@ -0,0 +1,56 @@
+package com.panli.sword.offer.day27._059II;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 队列的最大值
+ *
+ * @author lipan
+ * @date 2022-02-13
+ */
+public class MaxQueue {
+
+    Queue<Integer> q;
+    Deque<Integer> d;
+
+    public MaxQueue() {
+        q = new LinkedList<>();
+        d = new LinkedList<>();
+    }
+
+    public int max_value() {
+        if (d.isEmpty()) {
+            return -1;
+        }
+        return d.peekFirst();
+    }
+
+    public void push_back(int value) {
+        while (!d.isEmpty() && d.peekLast() < value) {
+            d.pollLast();
+        }
+        d.offerLast(value);
+        q.offer(value);
+    }
+
+    public int pop_front() {
+        if (q.isEmpty()) {
+            return -1;
+        }
+        int ans = q.poll();
+        if (ans == d.peekFirst()) {
+            d.pollFirst();
+        }
+        return ans;
+    }
+}
+
+/**
+ * Your MaxQueue object will be instantiated and called as such:
+ * MaxQueue obj = new MaxQueue();
+ * int param_1 = obj.max_value();
+ * obj.push_back(value);
+ * int param_3 = obj.pop_front();
+ */
